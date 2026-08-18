908821517c876857ca16f1c0ce9c1b714093a4aa
Christoffer <mech1803@gmail.com>
2026-08-18 21:15:39 +0200

    feat: add dotenv support, and improve config validation

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,10 +2,14 @@ import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { config } from "./config.js";
 import healthRoute from "./v1/routes/healthRoutes.js";
+import readyRoute from "./v1/routes/readyRoute.js";
 
 const app = new Hono();
 
 app.route("/v1", healthRoute);
+app.route("/v1", readyRoute);
+
+console.log(config.PORT);
 
 serve(
   {
--- apps/api/src/v1/routes/readyRoute.ts
+++ apps/api/src/v1/routes/readyRoute.ts
@@ -1,10 +1,27 @@
 import { Hono } from "hono";
-import pkg from "../../../package.json" with { type: "json" };
 
-const healthRoutes = new Hono();
+const readyRoute = new Hono();
 
-let hasConnection = true;
+readyRoute.get("/ready", async (c) => {
+  const url = process.env.DATABASE_HEALTH_URL!;
+  const apiKey = process.env.DATABASE_SECRET_KEY!;
+  const headers = new Headers();
+  headers.append("Content-Type", "application/json");
+  headers.append("apikey", apiKey);
 
-healthRoutes.get("/health", async (c) => {});
+  const config = {
+    method: "GET",
+    headers: headers,
+  };
 
-export default healthRoutes;
+  try {
+    const response = await fetch(url, config);
+    const data = await response.json();
+
+    return c.json(data.paths, 200);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+export default readyRoute;
